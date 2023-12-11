ed638b643d4f4c4db8ff4044b1831ef7a5259156
David McNeil <mcneil.david2@gmail.com>
2023-12-10 22:10:13 -0600

    Add MediaSearch type

--- glance-lib/src/store/media_sql.rs
+++ glance-lib/src/store/media_sql.rs
@@ -1,4 +1,5 @@
-use rusqlite::{named_params, Connection, Error};
+use derive_more::{From, Into};
+use rusqlite::{named_params, Connection, Error, Row, Statement};
 use time::OffsetDateTime;
 
 use super::converters::{FileFormatSql, HashSql, PathBufSql};
@@ -14,6 +15,17 @@ pub(crate) struct MediaSql {
     pub device: Option<String>,
     // pub iso: (),
     pub hash: HashSql,
+}
+
+#[derive(Debug, Default)]
+pub(crate) struct MediaFilter {
+    pub start: Option<OffsetDateTime>,
+    pub end: Option<OffsetDateTime>,
+}
+
+pub(crate) struct MediaSearch<'conn> {
+    statement: Statement<'conn>,
+    filter: MediaFilter,
 }
 
 impl MediaSql {
@@ -57,6 +69,34 @@ impl MediaSql {
         Ok(result)
     }
 
+    pub fn open_search<'conn>(conn: &'conn Connection) -> Result<MediaSearch<'conn>, Error> {
+        Self::search(conn, MediaFilter::default())
+    }
+
+    pub fn search<'conn>(
+        conn: &'conn Connection,
+        filter: MediaFilter,
+    ) -> Result<MediaSearch<'conn>, Error> {
+        let statement = match (filter.start, filter.end) {
+            (Some(_), Some(_)) =>
+                conn.prepare("SELECT (filepath, size, format, created, device, hash) FROM media \
+                    WHERE created >= ? AND created <= ? ORDER BY created",
+                )?,
+            (Some(_), None) =>                
+                conn.prepare("SELECT (filepath, size, format, created, device, hash) FROM media \
+                    WHERE created >= ? ORDER BY created",
+                )?, 
+            (None, Some(_)) =>
+                conn.prepare("SELECT (filepath, size, format, created, device, hash) FROM media \
+                    WHERE created <= ? ORDER BY created",
+                )?, 
+            (_, _) => conn.prepare(
+                "SELECT (filepath, size, format, created, device, hash) FROM media ORDER By created",
+                )?
+        };
+        Ok(MediaSearch { statement, filter })
+    }
+
     pub fn insert(&self, conn: &Connection) -> Result<(), Error> {
         let mut stmt = conn.prepare(
             "
@@ -74,5 +114,36 @@ impl MediaSql {
         })?;
 
         Ok(())
+    }
+
+    fn try_from_row(row: &Row<'_>) -> Result<MediaSql, Error> {
+        Ok(Self {
+            filepath: row.get(0)?,
+            size: row.get(1)?,
+            format: row.get(2)?,
+            created: row.get(3)?,
+            device: row.get(4)?,
+            hash: row.get(5)?,
+        })
+    }
+}
+
+impl<'conn> MediaSearch<'conn> {
+    fn new(statement: Statement<'conn>, filter: MediaFilter) -> Self {
+        Self { statement, filter }
+    }
+
+    fn iter(&'conn mut self) -> Result<impl Iterator<Item = MediaSql> + 'conn, Error> {
+        // let params = todo!("map the correct params");
+        let iter = self
+            .statement
+            .query_map([], |row| Ok(MediaSql::from(row)))?;
+        Ok(iter.map(|r| r.expect("all entries in `MediaSearch` are `Ok`")))
+    }
+}
+
+impl From<&Row<'_>> for MediaSql {
+    fn from(row: &Row) -> Self {
+        Self::try_from_row(row).expect("a valid select for `MediaSql`")
     }
 }
