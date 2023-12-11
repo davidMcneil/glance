0bd2e47891cf0568307bbbcc55aa94869820e0b6
David McNeil <mcneil.david2@gmail.com>
2023-12-11 06:32:52 -0600

    Cleanup query formatting

--- glance-lib/src/store/media_sql.rs
+++ glance-lib/src/store/media_sql.rs
@@ -1,8 +1,10 @@
-use derive_more::{From, Into};
+use const_format::formatcp;
 use rusqlite::{named_params, Connection, Error, Row, Statement};
 use time::OffsetDateTime;
 
 use super::converters::{FileFormatSql, HashSql, PathBufSql};
+
+const COLUMNS: &str = "(filepath, size, format, created, device, hash)";
 
 /// Low level type for interacting with media rows
 #[derive(Debug)]
@@ -19,8 +21,8 @@ pub(crate) struct MediaSql {
 
 #[derive(Debug, Default)]
 pub(crate) struct MediaFilter {
-    pub start: Option<OffsetDateTime>,
-    pub end: Option<OffsetDateTime>,
+    pub crated_start: Option<OffsetDateTime>,
+    pub created_end: Option<OffsetDateTime>,
 }
 
 pub(crate) struct MediaSearch<'conn> {
@@ -77,33 +79,33 @@ impl MediaSql {
         conn: &'conn Connection,
         filter: MediaFilter,
     ) -> Result<MediaSearch<'conn>, Error> {
-        let statement = match (filter.start, filter.end) {
-            (Some(_), Some(_)) =>
-                conn.prepare("SELECT (filepath, size, format, created, device, hash) FROM media \
-                    WHERE created >= ? AND created <= ? ORDER BY created",
-                )?,
-            (Some(_), None) =>
-                conn.prepare("SELECT (filepath, size, format, created, device, hash) FROM media \
-                    WHERE created >= ? ORDER BY created",
-                )?,
-            (None, Some(_)) =>
-                conn.prepare("SELECT (filepath, size, format, created, device, hash) FROM media \
-                    WHERE created <= ? ORDER BY created",
-                )?,
-            (_, _) => conn.prepare(
-                "SELECT (filepath, size, format, created, device, hash) FROM media ORDER By created",
-                )?
+        let statement = match (filter.crated_start, filter.created_end) {
+            (Some(_), Some(_)) => conn.prepare(formatcp!(
+                "SELECT {COLUMNS} FROM media \
+                    WHERE created >= :created_start \
+                        AND created <= :created_end \
+                    ORDER BY created",
+            ))?,
+            (Some(_), None) => conn.prepare(formatcp!(
+                "SELECT {COLUMNS} FROM media \
+                    WHERE created >= :created_start \
+                    ORDER BY created",
+            ))?,
+            (None, Some(_)) => conn.prepare(formatcp!(
+                "SELECT {COLUMNS} FROM media \
+                    WHERE created <= :created_end \
+                    ORDER BY created",
+            ))?,
+            (_, _) => conn.prepare(formatcp!("SELECT {COLUMNS} FROM media ORDER By created",))?,
         };
         Ok(MediaSearch { statement, filter })
     }
 
     pub fn insert(&self, conn: &Connection) -> Result<(), Error> {
-        let mut stmt = conn.prepare(
-            "
-        INSERT INTO media (filepath, size, format, created, device, hash)
-        VALUES (:filepath, :size, :format, :created, :device, :hash)
-    ",
-        )?;
+        let mut stmt = conn.prepare(formatcp!(
+            "INSERT INTO media {COLUMNS} \
+            VALUES (:filepath, :size, :format, :created, :device, :hash)"
+        ))?;
         stmt.insert(named_params! {
             ":filepath": self.filepath,
             ":size": self.size,
@@ -122,13 +124,16 @@ impl<'conn> MediaSearch<'conn> {
         Self { statement, filter }
     }
 
-    fn iter(
+    pub fn iter(
         &'conn mut self,
     ) -> Result<impl Iterator<Item = Result<MediaSql, Error>> + 'conn, Error> {
-        // let params = todo!("map the correct params");
+        let params = named_params! {
+            ":created_start": self.filter.crated_start,
+            ":created_end": self.filter.created_end,
+        };
         let iter = self
             .statement
-            .query_map([], |row| MediaSql::try_from(row))?;
+            .query_map(params, |row| MediaSql::try_from(row))?;
         Ok(iter)
     }
 }
