beb85d66e7fc9bcfdc880b3f33089d676fdc47ad
David McNeil <mcneil.david2@gmail.com>
2025-02-15 21:37:55 -0600

    Add basic dioxus

---
+++ glance-dioxus/README.md
@@ -0,0 +1,44 @@
+# Glance UI
+
+- large image
+    - image list underneath
+- list of images
+- skip 10 forward
+- random image
+- movie player
+- slide show
+- fullscreen
+- filter
+- show exif
+- edit exif
+- tag images
+- save image to folder
+- save link to folder
+- delete
+
+## Development
+
+Your new bare-bones project includes minimal organization with a single `main.rs` file and a few assets.
+
+### Tailwind
+1. Install npm: https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+2. Install the Tailwind CSS CLI: https://tailwindcss.com/docs/installation
+3. Run the following command in the root of the project to start the Tailwind CSS compiler:
+
+```bash
+npx tailwindcss -i ./input.css -o ./assets/tailwind.css --watch
+```
+
+### Serving Your App
+
+Run the following command in the root of your project to start developing with the default platform:
+
+```bash
+dx serve
+```
+
+To run for a different platform, use the `--platform platform` flag. E.g.
+```bash
+dx serve --platform desktop
+```
+
---
+++ glance-dioxus/src/main.rs
@@ -0,0 +1,76 @@
+use std::path::PathBuf;
+
+use dioxus::prelude::*;
+use walkdir::WalkDir;
+
+const FAVICON: Asset = asset!("/assets/favicon.ico");
+const MAIN_CSS: Asset = asset!("/assets/main.css");
+const HEADER_SVG: Asset = asset!("/assets/header.svg");
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
+fn main() {
+    dioxus::launch(App);
+}
+
+#[component]
+fn App() -> Element {
+    rsx! {
+        document::Link { rel: "icon", href: FAVICON }
+        document::Link { rel: "stylesheet", href: MAIN_CSS } document::Link { rel: "stylesheet", href: TAILWIND_CSS }
+        div { "testing"}
+        Hero {}
+    }
+}
+
+#[component]
+pub fn Hero() -> Element {
+    let images = use_signal(|| {
+        WalkDir::new("/home/david/Desktop/pics")
+            .into_iter()
+            .filter_map(|e| e.ok()) // Ignore errors
+            .filter(|e| e.file_type().is_file()) // Only get files
+            .filter_map(|e| e.path().to_str().map(|s| s.to_string()))
+            .collect::<Vec<_>>()
+    });
+    let mut index = use_signal(|| 0);
+
+    let mut previous = move || {
+        let mut i = index.write();
+        if *i == 0 {
+            *i = images.read().len() - 1; // Wrap around to last image
+        } else {
+            *i -= 1;
+        }
+    };
+
+    let mut next = move || {
+        let mut i = index.write();
+        *i = (*i + 1) % images.read().len(); // Wrap around to first image
+    };
+
+    let handle_key_down_event = move |evt: KeyboardEvent| match evt.key() {
+        Key::ArrowLeft => previous(),
+        Key::ArrowRight => next(),
+        _ => {}
+    };
+
+    rsx! {
+        div {
+            tabindex: "0",
+            onkeydown: handle_key_down_event,
+            div { "{images.read()[*index.read()]} {index}"}
+            img {
+                src: images.read()[index()].clone(),
+                style: "max-width: 80vw; max-height: 80vh; margin-top: 20px;"
+            }
+            // for (i, image) in images().iter().enumerate() {
+            //     img {
+            //         key: "{image}",
+            //         style: if index() == i { "display: block;" } else { "display: none;" },
+            //         src: images.read()[i].clone(),
+            //         style: "max-width: 80vw; max-height: 80vh; margin-top: 20px;"
+            //     }
+            // }
+        }
+    }
+}
